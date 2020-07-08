bb930d561ccb7d465232be567a936ab80a857c5a
Carolina Higuera <carohiguera92@gmail.com>
2020-07-08 11:13:39 -0500

    Added bikes dataset and dqn functions

---
+++ rl_funAprox.py
@@ -0,0 +1,102 @@
+from collections import deque
+import tensorflow as tf
+import numpy as np
+
+
+state_size = 4
+# Memory parameters
+memory_size = 10000            # memory capacity
+batch_size = 200                # experience mini-batch size
+pretrain_length = batch_size   # number experiences to pretrain the memory
+max_util = 200
+
+class QNetwork:
+    def __init__(self, learning_rate=0.01,action_size=2, hidden_size=10, name='QNetwork'):
+        # state inputs to the Q-network
+        with tf.variable_scope(name):
+            self.inputs_ = tf.placeholder(tf.float32, [None, state_size], name='inputs')
+            
+            # One hot encode the actions to later choose the Q-value for the action
+            self.actions_ = tf.placeholder(tf.int32, [None], name='actions')
+            one_hot_actions = tf.one_hot(self.actions_, action_size)
+            
+            # Target Q values for training
+            self.targetQs_ = tf.placeholder(tf.float32, [None], name='target')
+            
+            # ReLU hidden layers
+            self.fc1 = tf.contrib.layers.fully_connected(self.inputs_, hidden_size, activation_fn=tf.nn.relu)
+            self.fc2 = tf.contrib.layers.fully_connected(self.fc1, hidden_size, activation_fn=tf.nn.relu)
+
+            # Linear output layer
+            self.output = tf.contrib.layers.fully_connected(self.fc2, action_size, 
+                                                            activation_fn=None, scope='output')
+            
+            ### Train with loss (targetQ - Q)^2
+            # output has length 2, for two actions. This next line chooses
+            # one value from output (per row) according to the one-hot encoded actions.
+            self.Q = tf.reduce_sum(tf.multiply(self.output, one_hot_actions), axis=1)
+            
+            self.loss = tf.reduce_mean(tf.square(self.targetQs_ - self.Q))
+            self.opt = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
+
+
+class Memory():
+    def __init__(self, max_size = 1000):
+        self.buffer = deque(maxlen=max_size)
+    
+    def add(self, experience):
+        self.buffer.append(experience)
+            
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arange(len(self.buffer)), 
+                               size=batch_size, 
+                               replace=False)
+        return [self.buffer[ii] for ii in idx]
+
+
+def fill_memory(env, memory):
+    # Make a bunch of random actions and store the experiences
+    env.reset()
+    state, reward, done, _ = env.step(env.action_space.sample())
+    for ii in range(pretrain_length):
+        # Uncomment the line below to watch the simulation
+        # env.render()
+
+        # Make a random action
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+
+        if done:
+            # The simulation fails so no next state
+            next_state = np.zeros(state.shape)
+            # Add experience to memory
+            memory.add((state, action, reward, next_state))
+            
+            # Start new episode
+            env.reset()
+            # Take one random step to get the pole and cart moving
+            state, reward, done, _ = env.step(env.action_space.sample())
+        else:
+            # Add experience to memory
+            memory.add((state, action, reward, next_state))
+            state = next_state
+    return memory
+
+
+
+def smooth(x,window_len=11,window='hanning'):
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+    if window_len<3:
+        return x
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
